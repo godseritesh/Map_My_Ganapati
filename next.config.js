6a4b22a4a4143e3080467d1f297ee9d1ce6da569
Ritesh Godse <riteshgodse008@gmail.com>
2026-06-14 15:17:36 +0530

    fix: use CSP frame-ancestors to allow iframe embedding

--- next.config.js
+++ next.config.js
@@ -9,8 +9,8 @@ const nextConfig = {
         source: '/(.*)',
         headers: [
           {
-            key: 'X-Frame-Options',
-            value: 'ALLOWALL',
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors *",
           },
         ],
       },
